39a1e94c881b22b4226858a1a4f21310e18a06e5
Kyan Collier <r1036052@student.thomasmore.be>
2026-08-12 12:31:06 +0200

    First component with angular (not interactive yet, made to get used to angular)

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login'
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
